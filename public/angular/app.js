6398e8636a0f0bb72e8f92182c21422760ecfb92
Roxanne Lopez van Dooren <roxannelopez6@hotmail.com>
2014-12-21 17:23:07 +0100

    Added logo and created index look

---
+++ public/angular/app.js
@@ -0,0 +1,5 @@
+var hackthechat = angular.module('hackthechat', ['ngMaterial']);
+
+
+
+
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 
+//Declaracion de rutas
+app.use("/public", express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
+ res.sendfile('index.html',{ root: 'public/' }); 
+});
+
+app.get('/helloworld', function(req, res){
   res.send('<h1>Hello world</h1>');
 });
 
