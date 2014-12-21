16646ae0db02691a018bd4203b0a59f3e4694e7c
Roxanne Lopez van Dooren <roxannelopez6@hotmail.com>
2014-12-21 23:58:02 +0100

    Reinstalled socket io for the correct one. Implemented some functions of the chat - design undone

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 //Declaracion de rutas
 app.use("/public", express.static(__dirname + '/public'));
@@ -12,6 +13,15 @@ app.get('/', function(req, res){
 app.get('/helloworld', function(req, res){
   res.send('<h1>Hello world</h1>');
 });
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+
+});
+
 
 http.listen(8000, function(){
   console.log('listening on *:8000');
